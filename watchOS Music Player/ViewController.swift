c4301774d7e6ffc478400d770876581fdf55cca4
takpika <takpika0308@gmail.com>
2018-10-26 19:45:00 +0900

    Initial Commit

---
+++ watchOS Music Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  watchOS Music Player
+//
+//  Created by takumi saito on 2018/10/26.
+//  Copyright © 2018 takpika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
